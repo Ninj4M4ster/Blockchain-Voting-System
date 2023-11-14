9037076287d4e0709fbedcbeb89e91a24798503f
Ninj4M4ster <kuba.drzewiecki@icloud.com>
2023-11-14 21:22:22 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/blockchain_voting_system/BlockchainVotingSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.blockchain_voting_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlockchainVotingSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlockchainVotingSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/blockchain_voting_system/BlockchainVotingSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.blockchain_voting_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlockchainVotingSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
